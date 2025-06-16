44ffc7dc4a7b506be75387eb80bc6e54aa9590d2
Guilherme27398 <guilhermeramos@ipvc.pt>
2025-06-16 22:02:26 +0100

    .

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -4,117 +4,82 @@ export default class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        // Corrigido: nome do mapa JSON
-        this.load.tilemapTiledJSON('dungeon', 'Assets/maps/dungeon_map.json');
-
-        // Tileset imagem correta
+        this.load.tilemapTiledJSON('dungeon', 'Assets/maps/dungeonnewmap.json');
         this.load.image('Map01', 'Assets/images/tile/Map01.png');
-
-        // Spritesheet do jogador
-        this.load.spritesheet('adventurer', 'Assets/images/adventurer.png', {
-            frameWidth: 50,
-            frameHeight: 37
-        });
+        this.load.spritesheet('adventurer', 'Assets/images/adventurer.png', { frameWidth: 50, frameHeight: 37 });
+        this.load.spritesheet('adventurer_run', 'Assets/images/adventurer-run-Sheet.png', { frameWidth: 50, frameHeight: 37 });
     }
 
     create() {
-        // Cria o tilemap
         const map = this.make.tilemap({ key: 'dungeon' });
-
-        // Adiciona tileset (nome no Tiled e nome da imagem)
         const tileset = map.addTilesetImage('Map01', 'Map01');
+        this.blocks1 = map.createLayer('Camada de Blocos 1', tileset, 0, 0);
+        this.blocks2 = map.createLayer('Camada de Blocos 2', tileset, 0, 0);
+        this.blocks3 = map.createLayer('Camada de Blocos 3', tileset, 0, 0);
+        this.blocks4 = map.createLayer('Camada de Blocos 4', tileset, 0, 0);
 
-        // Cria camadas – nomes reais no JSON
-        map.createLayer('Camada de Blocos 1', tileset, 0, 0);
-        map.createLayer('Camada de Blocos 2', tileset, 0, 0);
-        map.createLayer('Camada de Blocos 3', tileset, 0, 0);
-        map.createLayer('Camada de Blocos 4', tileset, 0, 0);
-
-        // Player
         this.player = this.physics.add.sprite(32, 32, 'adventurer');
+        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
         this.player.setCollideWorldBounds(true);
 
-        // Câmera segue o jogador
         this.cameras.main.startFollow(this.player);
         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.setZoom(2.5);
 
-        // Teclado
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.wasd = this.input.keyboard.addKeys({ up: 'W', left: 'A', down: 'S', right: 'D' });
         this.attackKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
-        // Animações
         this.createAnimations();
+
+        // bloqueia movimento durante ataque
+        this.isAttacking = false;
+        this.player.on('animationcomplete-attack', () => {
+            this.isAttacking = false;
+        });
     }
 
     createAnimations() {
-        this.anims.create({
-            key: 'idle',
-            frames: this.anims.generateFrameNumbers('adventurer', { start: 0, end: 3 }),
-            frameRate: 5,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('adventurer', { start: 4, end: 7 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('adventurer', { start: 8, end: 11 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'up',
-            frames: this.anims.generateFrameNumbers('adventurer', { start: 12, end: 15 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'down',
-            frames: this.anims.generateFrameNumbers('adventurer', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'attack',
-            frames: this.anims.generateFrameNumbers('adventurer', { start: 16, end: 19 }),
-            frameRate: 12,
-            repeat: 0
-        });
+        this.anims.create({ key: 'idle', frames: this.anims.generateFrameNumbers('adventurer', { start: 0, end: 3 }), frameRate: 4, repeat: -1 });
+        this.anims.create({ key: 'left', frames: this.anims.generateFrameNumbers('adventurer', { start: 4, end: 7 }), frameRate: 6, repeat: -1 });
+        this.anims.create({ key: 'right', frames: this.anims.generateFrameNumbers('adventurer', { start: 8, end: 11 }), frameRate: 6, repeat: -1 });
+        this.anims.create({ key: 'up', frames: this.anims.generateFrameNumbers('adventurer', { start: 12, end: 15 }), frameRate: 6, repeat: -1 });
+        this.anims.create({ key: 'down', frames: this.anims.generateFrameNumbers('adventurer', { start: 20, end: 23 }), frameRate: 6, repeat: -1 });
+        this.anims.create({ key: 'run', frames: this.anims.generateFrameNumbers('adventurer_run', { start: 0, end: 5 }), frameRate: 8, repeat: -1 });
+        this.anims.create({ key: 'attack', frames: this.anims.generateFrameNumbers('adventurer', { start: 45, end: 55 }), frameRate: 6, repeat: 0 });
     }
 
-    update(time, delta) {
-        const speed = 0.2 * delta;
-        const player = this.player;
-
-        player.setVelocity(0);
-
-        if (Phaser.Input.Keyboard.JustDown(this.attackKey)) {
-            player.anims.play('attack', true);
+    update() {
+        if (this.isAttacking) {
+            this.player.setVelocity(0);
             return;
         }
 
-        if (this.cursors.left.isDown) {
-            player.setVelocityX(-speed);
-            player.anims.play('left', true);
-        } else if (this.cursors.right.isDown) {
-            player.setVelocityX(speed);
-            player.anims.play('right', true);
-        } else if (this.cursors.up.isDown) {
-            player.setVelocityY(-speed);
-            player.anims.play('up', true);
-        } else if (this.cursors.down.isDown) {
-            player.setVelocityY(speed);
-            player.anims.play('down', true);
+        const speed = 120;
+        this.player.setVelocity(0);
+
+        if (Phaser.Input.Keyboard.JustDown(this.attackKey)) {
+            this.isAttacking = true;
+            this.player.play('attack', true);
+            return;
+        }
+
+        if (this.cursors.left.isDown || this.wasd.left.isDown) {
+            this.player.setVelocityX(-speed);
+            this.player.anims.play('run', true);
+            this.player.setFlipX(true);
+        } else if (this.cursors.right.isDown || this.wasd.right.isDown) {
+            this.player.setVelocityX(speed);
+            this.player.anims.play('run', true);
+            this.player.setFlipX(false);
+        } else if (this.cursors.up.isDown || this.wasd.up.isDown) {
+            this.player.setVelocityY(-speed);
+            this.player.anims.play('up', true);
+        } else if (this.cursors.down.isDown || this.wasd.down.isDown) {
+            this.player.setVelocityY(speed);
+            this.player.anims.play('down', true);
         } else {
-            player.anims.play('idle', true);
+            this.player.anims.play('idle', true);
         }
     }
 }
